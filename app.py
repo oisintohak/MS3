80dd99885d6011d18d073f8d34ab0e2247316ff0
Oisín Tohak <oisintohak@gmail.com>
2023-01-13 17:14:31 +0000

    Update debug

--- app.py
+++ app.py
@@ -433,4 +433,4 @@ def error500(e):
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=os.environ.get('DEBUGGING'))
+            debug=os.environ.get('DEBUGGING', False))
